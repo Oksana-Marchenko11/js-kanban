3ea915a98fdb4bb9921ced5d5d614cf55830edee
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2024-06-09 19:57:41 +0300

    set project name to localStorage

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
+console.log("start");
 
+const form = document.querySelector(".form-modal");
+
+function onSubmit(e) {
+  console.log(e);
+  e.preventDefault();
+  value = form.elements.project_name.value;
+  localStorage.setItem("projectName", value.toString());
+}
+
+form.addEventListener("submit", onSubmit);
